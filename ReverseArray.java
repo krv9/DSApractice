8feb86c50f207576c540147d318cd5077ed8c6c5
krv9 <krishna.9vyas@gmail.com>
2024-06-08 13:02:24 +0530

    ReverseArray

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,11 +1,21 @@
+import java.util.Arrays;
+
 class ReverseArray {
-    public static void reverse(char[] a){
-        for ( int i=a.length-1; i>=0; i--){
-            System.out.print(a[i]+ " ");
+    public static void reverse(char[] a) {
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
         }
     }
+
+    public static void copy(char[] copy) {
+        System.out.println("copy of array in another one");
+        for (int i = 0; i < copy.length; i++) {
+            System.out.print(copy[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        char[] a = new char[] { 'k','i','a'};
+        char[] a = new char[] { 'k', 'i', 'a' };
         System.out.println("original array");
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
@@ -13,5 +23,9 @@ class ReverseArray {
         System.out.println();
         System.out.println("After reverse");
         reverse(a);
+        System.out.println();
+        char[] copy = Arrays.copyOf(a, 3);
+
+        copy(copy);
     }
 }
